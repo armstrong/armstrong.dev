27872f583e9bdce7c9ccf699983721d13fecda29
Travis Swicegood <development@domain51.com>
2011-05-05 17:14:06 -0500

    initial commit

---
+++ README.rst
@@ -0,0 +1,52 @@
+armstrong.dev
+=============
+Tools and such for handling development of Armstrong applications
+
+This package contains some of the various helpers needed to do development work
+on the Armstrong packages.  If you're not actively developing, or working with
+development versions of Armstrong, you probably don't need this package.
+
+Usage
+-----
+
+**TODO**
+
+
+Installation
+------------
+
+::
+
+    name="armstrong.dev"
+    pip install -e git://github.com/armstrongcms/$name#egg=$name
+
+
+Contributing
+------------
+
+* Create something awesome -- make the code better, add some functionality,
+  whatever (this is the hardest part).
+* `Fork it`_
+* Create a topic branch to house your changes
+* Get all of your commits in the new topic branch
+* Submit a `pull request`_
+
+
+License
+-------
+Copyright 2011 Bay Citizen and Texas Tribune
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+.. _pull request: http://help.github.com/pull-requests/
+.. _Fork it: http://help.github.com/forking/
---
+++ setup.py
@@ -0,0 +1,46 @@
+from distutils.core import setup
+import os
+
+# Borrowed and modified from django-registration
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir:
+    os.chdir(root_dir)
+
+def build_package(dirpath, dirnames, filenames):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+    pkg = dirpath.replace(os.path.sep, '.')
+    if os.path.altsep:
+        pkg = pkg.replace(os.path.altsep, '.')
+    packages.append(pkg)
+
+[build_package(dirpath, dirnames, filenames) for dirpath, dirnames, filenames in os.walk('armstrong')]
+
+setup(
+    name='armstrong.dev',
+    version='0.0.1a',
+    description='Tools needed for development and testing of Armstrong',
+    author='Bay Citizen and Texas Tribune',
+    author_email='all@armstrongcms.org',
+    url='http://github.com/armstrongcms/armstrong.dev/',
+    # TODO: generate this dynamically
+    packages=packages,
+    install_requires=[
+        # TODO: add this as a concrete dependency once 1.1 is out
+        #'fabric',
+    ],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
