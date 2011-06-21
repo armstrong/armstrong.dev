e2d80d36d425275da01d69f2f75d2d365959e9fc
Travis Swicegood <development@domain51.com>
2011-06-21 15:15:53 -0500

    Switch over to pkgutil for namespace declaration

--- armstrong/dev/tasks/__init__.py
+++ armstrong/dev/tasks/__init__.py
@@ -1,5 +1,5 @@
-import pkg_resources
-pkg_resources.declare_namespace(__name__)
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
 
 
 from contextlib import contextmanager
