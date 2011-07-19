c275a4d8ce84579057151b5d7dbcf457fbc3f235
Travis Swicegood <development@domain51.com>
2011-07-19 16:19:07 -0500

    fix some pep8 issues with whitespace at the end of lines

--- armstrong/dev/tasks/__init__.py
+++ armstrong/dev/tasks/__init__.py
@@ -109,14 +109,13 @@ def test():
         with html_coverage_report():
             unittest.TextTestRunner(verbosity=2).run(test_suite)
     else:
-        error = '''The fabfile must either contain django settings or 
-                    supply a test_suite. See the armstrong.apps.audio 
-                    fabfile.py for a example of testing a django app. 
-                    See armstrong.apps.audio.backends.id3reader fabfile.py 
+        error = '''The fabfile must either contain django settings or
+                    supply a test_suite. See the armstrong.apps.audio
+                    fabfile.py for a example of testing a django app.
+                    See armstrong.apps.audio.backends.id3reader fabfile.py
                     for a example of running a simple test suite'''
         raise ImproperlyConfigured(error)
-       
-    
+
 
 @task
 def runserver():
