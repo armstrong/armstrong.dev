dbc518b61a5e5b95f266db73e7f081935ff2706c
Jon Cotton <jcotton@baycitizen.org>
2013-04-19 16:02:10 -0700

    Iterate the last commit. `dev_django.py` is now the hub for running a Django environment with the component's settings; it's not just for tests.
    - `run_django_cmd()` can run any Django command
    - command line access proxies `manage.py` (so it too can run any Django command)
    - add loading error messages
    - `test` is a special case where a) we want to allow specific tests (https://docs.djangoproject.com/en/1.5/topics/testing/overview/#running-tests) b) but default to `TESTED_APPS`

--- armstrong/dev/default_settings.py
+++ armstrong/dev/default_settings.py
@@ -9,26 +9,17 @@ package = json.load(open("./package.json"))
 #
 # Default settings
 #
-DEBUG = True
 TESTED_APPS = ['tests']
+INSTALLED_APPS = [package["name"], 'tests']
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3"
     }
 }
 
-
 #
-# Component specific settings
+# A component may override settings by creating an `env_settings.py`
+# file in its root directory that imports from this file.
 #
-# Each component needs to create an `env_settings.py` file in its
-# root directory that imports this file. Then the component can define
-# whatever it needs for its Django environment. example:
-#
-# from armstrong.dev.default_settings import *
-#
-# INSTALLED_APPS = [
-#     package["name"],
-#     '%s.tests' % package["name"],
-# ]
+#     from armstrong.dev.default_settings import *
 #
