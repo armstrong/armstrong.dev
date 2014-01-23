ad0efe64baf68fa1d8ef95ce56c13f407c5f0d0e
Jon Cotton <jc@finaltransfer.net>
2014-01-23 11:45:28 -0800

    slight improvement on `fudge` detection

--- armstrong/dev/tests/utils/base.py
+++ armstrong/dev/tests/utils/base.py
@@ -2,22 +2,19 @@ from django.test import TestCase as DjangoTestCase
 from django.db import models
 
 # DEPRECATED remove when we drop Django 1.3 support
-# Backport override_settings from Django 1.4
 try:
     from django.test.utils import override_settings
 except ImportError:
     from .backports import override_settings
 
-# If the component uses fudge, provide useful shared behavior
-try:
+try:  # If the component uses fudge, provide useful shared behavior
     import fudge
-    hasFudge = True
 except ImportError:
-    hasFudge = False
+    fudge = False
 
 
 class ArmstrongTestCase(DjangoTestCase):
-    if hasFudge:
+    if fudge:
         def setUp(self):
             fudge.clear_expectations()
             fudge.clear_calls()
